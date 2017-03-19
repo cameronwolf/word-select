e470df1b765987d05f6f034e037d4bce14abd3a3
cwolf1038 <cwolf123456@yahoo.com>
2017-03-18 23:51:00 -0400

    Added tests

---
+++ test/test.js
@@ -0,0 +1,32 @@
+// __          __           _    _____      _           _   
+// \ \        / /          | |  / ____|    | |         | |  
+//  \ \  /\  / /__  _ __ __| | | (___   ___| | ___  ___| |_ 
+//   \ \/  \/ / _ \| '__/ _` |  \___ \ / _ \ |/ _ \/ __| __|
+//    \  /\  / (_) | | | (_| |  ____) |  __/ |  __/ (__| |_ 
+//     \/  \/ \___/|_|  \__,_| |_____/ \___|_|\___|\___|\__|
+//                                                          
+//*****************************************************************************
+'use strict';
+//*****************************************************************************
+
+const chai = require('chai');
+const expect = chai.expect;
+
+//*****************************************************************************
+
+const wordSelect = require('../index.js');
+
+//*****************************************************************************
+describe('word-select', function(){
+
+	it('should return a string when given a string, and a valid word is requested', function() {
+		expect( wordSelect.select('First second third', 2)).to.equal('second');
+	});
+	it('should return false if the nth word does not exist', function() {
+		expect( wordSelect.select('First second third', 4)).to.be.false;
+	});
+	it('should return false when the input is not a string', function() {
+		expect( wordSelect.select(1, 2)).to.be.false;
+	});
+
+});
