3d2e8fd9fb2851a6a452ea08526abc83672f9c34
cwolf1038 <cwolf123456@yahoo.com>
2017-03-19 00:35:05 -0400

    Added select function

---
+++ index.js
@@ -0,0 +1,43 @@
+// __          __           _    _____      _           _   
+// \ \        / /          | |  / ____|    | |         | |  
+//  \ \  /\  / /__  _ __ __| | | (___   ___| | ___  ___| |_ 
+//   \ \/  \/ / _ \| '__/ _` |  \___ \ / _ \ |/ _ \/ __| __|
+//    \  /\  / (_) | | | (_| |  ____) |  __/ |  __/ (__| |_ 
+//     \/  \/ \___/|_|  \__,_| |_____/ \___|_|\___|\___|\__|
+//                                                          
+//*****************************************************************************
+'use strict';
+//*****************************************************************************
+
+function removeWhiteSpace(array){
+
+	let i = 0;
+	let length = array.length;
+	for (i = 0;i < length;i++){
+		if (array[i] === ''){
+			array.splice(i, 1);
+			i--;
+			length--;
+		}
+	}
+
+}
+const select = function (input, num){
+
+	if (typeof input !== 'string'){
+		return false;
+	}
+	else {
+		const words = input.split(' ');
+
+		if (words.length < num){
+			return false;
+		}
+		else {
+			removeWhiteSpace(words);
+			return words[num - 1];
+		}
+	}
+};
+
+module.exports.select = select;
